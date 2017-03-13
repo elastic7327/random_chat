599b47d01d9249427f1bf8293d68d4ff3a3c7523
elastic7327 <elastic7327@gamil.commit>
2017-03-13 16:37:14 +0800

    first commit

---
+++ chat/script.sh
@@ -0,0 +1,4 @@
+python manage.py makemigrations --settings=chat.settings.development
+python manage.py migrate --settings=chat.settings.development
+python manage.py runserver --settings=chat.settings.development
+
